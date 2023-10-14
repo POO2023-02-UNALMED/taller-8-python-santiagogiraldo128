5ae12bf7ccf2dda05abaab5c0e2ca21cba30ca7a
santiagogiraldo128 <santiagogiraldo128@gmail.com>
2023-10-13 20:49:09 -0500

    algunas correcciones mas

--- futbolista.py
+++ futbolista.py
@@ -5,9 +5,9 @@ class Futbolista(Persona, Deportista):
 
     listaFutbolistas = []
 
-    def __init__(self, nombre, edad, altura, sexo, deporte = "Futbol", añosPracticando = 0, golesMarcados = 0, tarjetasRojas = 0, piernaHabil = "Derecha"):
+    def __init__(self, nombre, edad, altura, sexo, añosPracticando = 0, golesMarcados = 0, tarjetasRojas = 0, piernaHabil = "Derecha"):
         super().__init__(nombre, edad, altura, sexo)
-        Deportista.__init__(self, deporte, añosPracticando)
+        Deportista.__init__(self, "Futbol", añosPracticando)
         self._golesMarcados = golesMarcados
         self._tarjetasRojas = tarjetasRojas
         self._piernaHabil = piernaHabil
@@ -30,6 +30,14 @@ class Futbolista(Persona, Deportista):
     
     def setPiernaHabil(self, piernaHabil):
         self._piernaHabil = piernaHabil
+
+    @classmethod
+    def getListaFutbolistas(cls):
+        return cls.listaFutbolistas
+
+    @classmethod
+    def setListaFutbolistas(cls, lista):
+        cls.listaFutbolistas = lista
 
     def __str__(self):
         return "Mi nombre es " + self._nombre + " soy profesional en el deporte " + self._deporte + " tengo " + str(self._edad) + " años de edad y llevo " + str(self._añosPracticando) + " años en el deporte"
